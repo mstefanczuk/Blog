4781c682a75400a06c789052fe2f2ec82f814073
Michał Stefańczuk <michal.stefanczuk@gmail.com>
2018-11-18 22:48:53 +0100

    Add author image an fix main class unit test

--- src/test/java/pl/stefanprogramuje/BlogApplicationTests.java
+++ src/test/java/pl/stefanprogramuje/BlogApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.stefanprogramuje.blog.BlogApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = BlogApplication.class)
 public class BlogApplicationTests {
 
 	@Test
